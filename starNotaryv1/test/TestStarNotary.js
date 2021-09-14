874374c222eaaa545e1a0fff94f28f1503e5111f
linnie <omgjc71@gmail.com>
2021-09-14 17:18:41 -0400

    starNotary

---
+++ starNotaryv1/test/TestStarNotary.js
@@ -0,0 +1,34 @@
+// Importing the StartNotary Smart Contract ABI (JSON representation of the Smart Contract)
+const StarNotary = artifacts.require("StarNotary");
+
+var accounts; // List of development accounts provided by Truffle
+var owner; // Global variable use it in the tests cases
+
+// This called the StartNotary Smart contract and initialize it
+contract('StarNotary', (accs) => {
+    accounts = accs; // Assigning test accounts
+    owner = accounts[0]; // Assigning the owner test account
+});
+
+// Example test case, it will test if the contract is able to return the starName property 
+// initialized in the contract constructor
+it('has correct name', async () => {
+    let instance = await StarNotary.deployed(); // Making sure the Smart Contract is deployed and getting the instance.
+    let starName = await instance.starName.call(); // Calling the starName property
+    assert.equal(starName, "Awesome Neb Star"); // Assert if the starName property was initialized correctly
+});
+
+it('can be claimed', async () => {
+  let instance = await StarNotary.deployed();
+  await instance.claimStar({from: owner});
+  assert.equal(await instance.starOwner.call(), owner)
+})
+
+it('it can change owners', async () => {
+  var secondUser = accounts[1];
+  let instance = await StarNotary.deployed();
+  await instance.claimStar({from: owner});
+  assert.equal(await instance.starOwner.call(), owner);
+  await instance.claimStar({from: secondUser});
+  assert.equal(await instance.starOwner.call(), secondUser)
+})
